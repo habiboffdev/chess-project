3485f2d5632c04a3d78467ef08a4c4d5292b36d7
Mirzosharif Habibov <mirzoupwork@gmail.com>
2024-07-18 17:34:56 +0500

    Fixing some problems

--- Users/views.py
+++ Users/views.py
@@ -5,7 +5,6 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import status, serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer 
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from .serializers import UserSerializer, UserChangeSerializer
 from Users.models import User
@@ -19,7 +18,6 @@ class Home(APIView):
         content = {'message': 'Hello, World!'}
         return Response(content)
 class CustomTokenRefreshSerializer(TokenRefreshSerializer):
-
     def validate(self, attrs):
         # Validate the refresh token as usual
         data = super().validate(attrs)
@@ -38,30 +36,59 @@ class CustomTokenRefreshSerializer(TokenRefreshSerializer):
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
     def validate(self, attrs):
-        # The original validate method returns a dictionary with the tokens if the user is authenticated
+        """
+        Validates the user's attributes and returns the token data if the user is authenticated.
+
+        Args:
+            attrs (dict): The user's attributes.
+
+        Raises:
+            serializers.ValidationError: If the user account is inactive.
+
+        Returns:
+            dict: The token data if the user is active.
+        """
         data = super().validate(attrs)
         
-        # Check if the user is active
         if not self.user.is_active:
             raise serializers.ValidationError('User account is inactive.')
         
-        # If the user is active, return the token data
         return data
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def register_user(request):
-    print(request.data)
-    if request.method == 'POST':
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    """
+    API view to register a new user.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        Response: The HTTP response object.
+    """
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes([IsAuthenticated, IsAdminUser])
 def manage_user(request, user_id):
+    """
+    API view to manage a user.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+        user_id (int): The ID of the user to manage.
+
+    Returns:
+        Response: The HTTP response object.
+
+    Raises:
+        User.DoesNotExist: If the user with the specified ID does not exist.
+    """
     try:
         user = User.objects.get(id=user_id)
     except User.DoesNotExist:
